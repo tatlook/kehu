d4ae72cd85c4e8685086c6b6175c5eb70afa654e
Tatlook <youzhezhen@gmail.com>
2023-03-03 18:41:57 +0200

    add unit test of Token

---
+++ test/token-test.cpp
@@ -0,0 +1,86 @@
+/* 
+ * Kehu - a toe language
+ * Copyright (C) 2023 Zhen You Zhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h>
+#include <kehu/token.h>
+
+using namespace kehu::token;
+
+TEST(Token, OperatorEquals)
+{
+        Token t1 = {
+                .linec = 0,
+                .type = TOKEN_CHAR,
+                .value = '"',
+        };
+        Token t2 = {
+                .linec = 0,
+                .type = TOKEN_CHAR,
+                .value = '"',
+        };
+        ASSERT_TRUE(t1 == t2);
+        ASSERT_FALSE(t1 != t2);
+        t2 = {
+                .linec = 0,
+                .type = TOKEN_STRING,
+                .value = "\"",
+        };
+        ASSERT_FALSE(t1 == t2);
+        ASSERT_TRUE(t1 != t2);
+}
+
+TEST(Token, ToString)
+{
+        Token t = {
+                .linec = 0,
+                .type = TOKEN_INTEGER,
+                .value = '"',
+        };
+        ASSERT_THROW(t.operator std::string(),
+                        std::bad_variant_access);
+        t = {
+                .linec = 0,
+                .type = TOKEN_CHAR,
+                .value = '"',
+        };
+        ASSERT_EQ(t.operator std::string(), "'\"'");
+        t = {
+                .linec = 0,
+                .type = TOKEN_STRING,
+                .value = "voi saatana",
+        };
+        ASSERT_EQ(t.operator std::string(), "\"voi saatana\"");
+        t = {
+                .linec = 0,
+                .type = TOKEN_IDENTIFIER,
+                .value = "voi saatana",
+        };
+        ASSERT_EQ(t.operator std::string(), "voi saatana");
+        t = {
+                .linec = 0,
+                .type = TOKEN_INTEGER,
+                .value = 3000L,
+        };
+        ASSERT_EQ(t.operator std::string(), "3000");
+        t = {
+                .linec = 0,
+                .type = TOKEN_SYMBOL,
+                .value = ',',
+        };
+        ASSERT_EQ(t.operator std::string(), ",");
+}
